5fe5b2041d3190f5e0975f8057112de84c48619f
polinafirstova <p.i.firstova@gmail.com>
2024-05-25 16:22:29 +0700

    q

--- front/frontend/src/http/index.js
+++ front/frontend/src/http/index.js
@@ -8,25 +8,26 @@ const $api = axios.create({
 })
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Token ${localStorage.getItem('token')}`
+    config.headers.Authorization = `Token ${localStorage.getItem('auth_token')}`
     return config
 })
 
-$api.interceptors.response.use((config) => {
-    return config
-}, async (error) => {
-    const originalRequest = error.config
-    if (error.response.status == 401 && error.config && !error.config._isRetry) {
-        originalRequest._isRetry = true
-        try {
-            const response = await axios.post(`${API_URL}/auth/jwt/refresh`, { withCredentials: true })
-            localStorage.setItem('token', response.data.accessToken)
-            return $api.request(originalRequest)
-        } catch (e) {
-            console.log('Пользователь не авторизован')
-        }
-    }
-    throw error
-})
+// $api.interceptors.response.use((config) => {
+//     return config
+// }, async (error) => {
+//     const originalRequest = error.config
+//     if (error.response.status == 401 && error.config && !error.config._isRetry) {
+//         originalRequest._isRetry = true
+//         try {
+//             const response = await axios.post(`${API_URL}/auth/token/login/`, { withCredentials: true })
+//             // const response = await axios.post(`${API_URL}/auth/jwt/refresh`, { withCredentials: true })
+//             localStorage.setItem('auth_token', response.data.auth_token)
+//             return $api.request(originalRequest)
+//         } catch (e) {
+//             console.log('Пользователь не авторизован')
+//         }
+//     }
+//     throw error
+// })
 
 export default $api
