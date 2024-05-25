47b25ff6e75ec20e8bb4bc358f54f2698ef64022
Vyacheslav <vas134@tpu.ru>
2024-05-25 14:57:58 +0700

    more fixes

---
+++ front/frontend/src/routes.js
@@ -0,0 +1,15 @@
+export const routes = [
+    { path: '/', name: 'Главная' },
+    { path: '/home', name: 'Главная' },
+    { path: '/recommendations', name: 'Рекомендации' },
+    { path: '/recommendations/tops', name: 'Топы' },
+    { path: '/recommendations/collections', name: 'Подборки' },
+    { path: '/catalog', name: 'Каталог' },
+    { path: '/favorites', name: 'Избранное' },
+    { path: '/account/reader', name: 'Читатель' },
+    { path: '/account/reader/bookhistory', name: 'История книг' },
+    { path: '/account/librarian', name: 'Библиотекарь' },
+    { path: '/authorisation', name: 'Авторизация' },
+    { path: '/authorisation/forget_password', name: 'Забыли пароль' },
+    { path: '/registration', name: 'Регистрация' },
+]
