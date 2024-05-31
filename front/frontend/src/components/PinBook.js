281db0723926bc547658332b4d99e0b72ef374d5
polinafirstova <p.i.firstova@gmail.com>
2024-05-31 14:38:54 +0700

    q

---
+++ front/frontend/src/components/PinBook.js
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from 'react'
+import Button from '../components/Button'
+import useScrollBar from '../hooks/use-scrollBar'
+import Input from './Input'
+import UserService from '../service/UserService'
+import UseGetArray from '../hooks/use-getArray'
+
+export default function PinBook(props) {
+    const [readers, setReaders] = UseGetArray('api/v1/books/readers')
+    // [{ id: 1, name: 'kjnj' }, { id: 2, name: 'kjnj' }, { id: 3, name: 'kjnj' }, { id: 4, name: 'kjnj' }, { id: 5, name: 'kjnj' }, { id: 6, name: 'kjnj' }]
+
+    const [value, setValue] = useState('')
+    let selectedItems = []
+    const hasScroll = readers.length > 5
+    const filters = useRef(null)
+
+    useScrollBar(filters, hasScroll)
+
+    const filter = readers.filter(el => {
+        return el.name?.toLowerCase()?.includes(value.toLowerCase())
+        // return el.last_name?.toLowerCase()?.includes(value.toLowerCase()) || el.first_name?.toLowerCase()?.includes(value.toLowerCase())
+    })
+
+    async function handleCheckboxChange(el) {
+        const isChecked = selectedItems.includes(el)
+        const promise = new Promise((resolve) => {
+            if (isChecked) {
+                selectedItems = selectedItems.filter(item => item !== el)
+                const interval = setInterval(() => {
+                    if (!selectedItems.includes(el)) {
+                        clearInterval(interval)
+                        resolve()
+                    }
+                }, 100)
+            } else {
+                selectedItems.push(el)
+                const interval = setInterval(() => {
+                    if (selectedItems.includes(el)) {
+                        clearInterval(interval)
+                        resolve()
+                    }
+                }, 100)
+            }
+        })
+        await promise
+    }
+
+    return (
+        <div className='pin-book-background'>
+            <div className={`${props.className}`}>
+                <h1 className='pin-book-name'>Закрепление книги</h1>
+                <div className='catalog-filters-item pin-book-readers'
+                >
+                    <span className='catalog-filters-item-title'>Список читателей</span>
+                    <Input placeholder='Введите имя читателя'
+                        onChange={(e) => { setValue(e.target.value) }} />
+                    <div style={{
+                        height: hasScroll ? '155px' : 'auto',
+                        paddingRight: '10px',
+                    }}
+                        ref={filters} >
+                        <div className='catalog-filters-item-checkbox-item'>
+                            {filter.map(el => (
+                                <label key={el.id} className='catalog-filters-item-checkbox-label'
+                                // onClick={() => UserService.pinBook(props.bookId, el.id)}
+                                >
+                                    <input
+                                        type='checkbox'
+                                        className='catalog-filters-item-checkbox'
+                                        onChange={() => handleCheckboxChange(el.id)}
+                                    />
+                                    <span className='catalog-filters-item-checkbox-custom'></span>
+                                    {el.name}
+                                </label>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <Button
+                    title={'Закрепить'}
+                    onClick={() => UserService.pinBook(props.bookId, selectedItems[0])} />
+            </div>
+        </div >
+    )
+}
--- front/frontend/src/service/UserService.js
+++ front/frontend/src/service/UserService.js
@@ -9,17 +9,17 @@ export default class UserService {
         return $api.get(`/api/v1/books/${id}`)
     }
 
-    static async getAuthors() {
-        return $api.get('/api/v1/books/authors')
-    }
+    // static async getAuthors() {
+    //     return $api.get('/api/v1/books/authors')
+    // }
 
-    static async getGenres() {
-        return $api.get('/api/v1/books/genres')
-    }
+    // static async getGenres() {
+    //     return $api.get('/api/v1/books/genres')
+    // }
 
-    static async getTags() {
-        return $api.get('/api/v1/books/tags')
-    }
+    // static async getTags() {
+    //     return $api.get('/api/v1/books/tags')
+    // }
 
     static async getTops() {
         return $api.get('')
@@ -35,5 +35,12 @@ export default class UserService {
 
     static async getFavorites(id) {
         return $api.get('', { id })
+    }
+
+    static async pinBook(bookId, readerId) {
+        const auth_token = localStorage.getItem('auth_token')
+        return $api.post('/api/v1/books/pinbook/', { bookId, readerId },
+            { headers: { "Authorization": `Token ${auth_token}` } }
+        )
     }
 }
