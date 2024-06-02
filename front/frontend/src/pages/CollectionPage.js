6a73e87c0c5e048b93f88d94f3b117bf7462937b
polinafirstova <p.i.firstova@gmail.com>
2024-06-02 21:09:55 +0700

    q

--- front/frontend/src/pages/CollectionPage.js
+++ front/frontend/src/pages/CollectionPage.js
@@ -13,6 +13,7 @@ export default function CollectionPage() {
         async function getCollection() {
             try {
                 const response = await UserService.getCollectionsItem(id)
+                console.log(response)
                 setCollection(response.data)
             } catch (e) {
                 console.log(e)
@@ -31,17 +32,21 @@ export default function CollectionPage() {
     // }
 
     return (
-        <div>
-            <Path current={collection.title} />
-            <h1 className='collection-name'>{collection.title}</h1>
-            <p className='collection-description'>{collection.description}</p>
-            <div className='book-list'>
-                {collection.books.map(el => (
-                    <Link to={`${el.id}`} className='book-link-another-page' key={el.id}>
-                        <BookItem key={el.id} bookItem={el} />
-                    </Link>
-                ))}
-            </div>
-        </div>
+        <>
+            {collection !== null &&
+                <div>
+                    <Path current={collection.title} />
+                    <h1 className='collection-name'>{collection.title}</h1>
+                    <p className='collection-description'>{collection.description}</p>
+                    <div className='book-list'>
+                        {collection.books.map(el => (
+                            <Link to={`${el.id}`} className='book-link-another-page' key={el.id}>
+                                <BookItem key={el.id} bookItem={el} />
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            }
+        </>
     )
 }
