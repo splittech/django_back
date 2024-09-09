70269ecea41c5f2d308f2b2708c82a58e70efa0e
Vyacheslav <vas134@tpu.ru>
2024-09-09 16:17:41 +0700

    reviews and pinbook

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Tag, Author, Genre, Book, Review
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+    # Создание базовых объектов для тестирования
+        self.tag = Tag.objects.create(name="Fantasy", url="fantasy")
+        self.genre = Genre.objects.create(name="Adventure", url="adventure")
+        self.author = Author.objects.create(name="J.K. Rowling")
+        self.book = Book.objects.create(
+            title="Harry Potter",
+            description="A fantasy novel",
+            author=self.author,
+            genres=None,
+            tags=None,
+            url="harry-potter",
+            copies=0,
+            rating=0
+        )
+    def test_tag_creation(self):
+        self.assertEqual(str(self.tag), "Fantasy")
+        self.assertEqual(self.tag.url, "fantasy")
+
+    def test_genre_creation(self):
+        self.assertEqual(str(self.genre), "Adventure")
+        self.assertEqual(self.genre.url, "adventure")
+
+    def test_author_creation(self):
+        self.assertEqual(str(self.author), "J.K. Rowling")
+
+    def test_book_creation(self):
+        self.assertEqual(str(self.book), "Harry Potter")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import Group
+from users.models import User
 
-# Create your tests here.
+
+class SimpleAPITests(APITestCase):
+    def setUp(self):
+        # Получаем или создаем группу "Readers"
+        self.readers_group, created = Group.objects.get_or_create(name='Readers')
+
+        # Создаем пользователя и добавляем его в группу "Readers"
+        self.reader_user = User.objects.create_user(
+            username='reader',
+            password='testpassword',
+            email='reader@example.com'
+        )
+        self.reader_user.groups.add(self.readers_group)
+
+        # Получаем токен для аутентификации
+        self.token = self.get_token(self.reader_user)
+
+    def get_token(self, user):
+        # Получаем JWT токен для пользователя
+        response = self.client.post('/auth/jwt/create/', {'username': user.username, 'password': 'testpassword'})
+        return response.data['access']
+
+    def test_get_readers_list(self):
+        # Устанавливаем заголовок авторизации
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        # Отправляем запрос к API для получения списка читателей
+        response = self.client.get('http://127.0.0.1:8000/api/v1/books/readers')
+        # Проверяем статус ответа
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Проверяем, что список читателей не пуст
+        self.assertGreater(len(response.data), 0)
+
+# def test_get_librarians_list(self):
+# # Устанавливаем заголовок авторизации
+# self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+#
+# # Отправляем запрос к API для получения списка библиотекарей
+# response = self.client.get('/api/v1/users/librarians/')
+#
+# # Проверяем статус ответа
+# self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+# # Проверяем, что список библиотекарей не пуст
+# self.assertGreater(len(response.data), 0)
