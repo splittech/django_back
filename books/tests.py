ad759e2b62aba1dfdc7460371e2698d62195b967
Vyacheslav <vas134@tpu.ru>
2024-09-09 20:22:12 +0700

    reviews and pinbook

--- books/tests.py
+++ books/tests.py
@@ -10,12 +10,12 @@ from .models import Tag, Author, Genre, Book, Review
 class BookCreationTestCase(TestCase):
     def setUp(self):
         # Создание базовых объектов для тестирования
-        self.tag = Tag.objects.create(name="Fantasy", url="fantasy")
-        self.genre = Genre.objects.create(name="Adventure", url="adventure")
-        self.author = Author.objects.create(name="J.K. Rowling")
+        self.tag = Tag.objects.create(name="Приключение", url="prikluchenie")
+        self.genre = Genre.objects.create(name="Фэнтези", url="fentesy")
+        self.author = Author.objects.create(name="Джоан Роулинг")
         self.book = Book.objects.create(
-            title="Harry Potter",
-            description="A fantasy novel",
+            title="Гарри Поттер",
+            description="Какое-то описание",
             author=self.author,
             url="harry-potter",
             copies=10,
@@ -26,25 +26,25 @@ class BookCreationTestCase(TestCase):
 
     # Проверка созданных элементов
     def test_tag_creation(self):
-        self.assertEqual(str(self.tag), "Fantasy")
-        self.assertEqual(self.tag.url, "fantasy")
+        self.assertEqual(str(self.tag), "Приключение")
+        self.assertEqual(self.tag.url, "prikluchenie")
 
     def test_genre_creation(self):
-        self.assertEqual(str(self.genre), "Adventure")
-        self.assertEqual(self.genre.url, "adventure")
+        self.assertEqual(str(self.genre), "Фэнтези")
+        self.assertEqual(self.genre.url, "fentesy")
 
     def test_author_creation(self):
-        self.assertEqual(str(self.author), "J.K. Rowling")
+        self.assertEqual(str(self.author), "Джоан Роулинг")
 
     def test_book_creation(self):
-        self.assertEqual(str(self.book), "Harry Potter")
+        self.assertEqual(str(self.book), "Гарри Поттер")
 
     # Проверка книги на соответствие заданным параметрам
     def test_book_same(self):
         self.assertIn(self.genre, self.book.genres.all())
         self.assertIn(self.tag, self.book.tags.all())
-        self.assertEqual(str(self.book.author), "J.K. Rowling")
-        self.assertEqual(str(self.book.title), "Harry Potter")
+        self.assertEqual(str(self.book.author), "Джоан Роулинг")
+        self.assertEqual(str(self.book.title), "Гарри Поттер")
         self.assertEqual(str(self.book.url), "harry-potter")
         self.assertEqual(str(self.book.copies), "10")
         self.assertEqual(str(self.book.rating), "0")
@@ -52,13 +52,13 @@ class BookCreationTestCase(TestCase):
 
 class BookReviewsTestCase(APITestCase):
     def setUp(self):
-        # Создание книги
-        self.tag = Tag.objects.create(name="Fantasy", url="fantasy")
-        self.genre = Genre.objects.create(name="Adventure", url="adventure")
-        self.author = Author.objects.create(name="J.K. Rowling")
+        # Создание базовых объектов для тестирования
+        self.tag = Tag.objects.create(name="Приключение", url="prikluchenie")
+        self.genre = Genre.objects.create(name="Фэнтези", url="fentesy")
+        self.author = Author.objects.create(name="Джоан Роулинг")
         self.book = Book.objects.create(
-            title="Harry Potter",
-            description="A fantasy novel",
+            title="Гарри Поттер",
+            description="Какое-то описание",
             author=self.author,
             url="harry-potter",
             copies=10,
@@ -66,6 +66,7 @@ class BookReviewsTestCase(APITestCase):
         )
         self.book.genres.add(self.genre)
         self.book.tags.add(self.tag)
+
         # Создаем группы
         self.readers_group, created = Group.objects.get_or_create(name='Librarians')
         self.readers_group, created = Group.objects.get_or_create(name='Readers')
@@ -78,25 +79,21 @@ class BookReviewsTestCase(APITestCase):
         )
         self.reader_user.groups.add(self.readers_group)
 
-        # Получаем токен
-        self.token = self.get_token(self.reader_user)
-
-    def get_token(self, user):
         # Получаем токен для пользователя через запрос
-        response = self.client.post('/auth/token/login/', {'username': user.username, 'password': 'testpassword'})
-        return response.data['auth_token']
+        response = self.client.post('/auth/token/login/', {'username': self.reader_user.username, 'password': 'testpassword'})
+        self.token = response.data['auth_token']
 
     def test_book_rating_changed(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        # Отправляем запрос к API для получения списка читателей
+        # Через запрос добавляем два отзыва к ранее созданной книге
         response = self.client.post('http://127.0.0.1:8000/api/v1/books/reviews/create/', {
             'rating': '2', 'name': 'Плохо', 'text': 'Не понравилось', 'author': str(self.reader_user.pk),
             'book': str(self.book.pk)
         })
         response = self.client.post('http://127.0.0.1:8000/api/v1/books/reviews/create/', {
-            'rating': '6', 'name': 'Плохо', 'text': 'Не понравилось', 'author': str(self.reader_user.pk),
+            'rating': '6', 'name': 'Хорошо', 'text': 'Понравилось', 'author': str(self.reader_user.pk),
             'book': str(self.book.pk)
         })
         self.book.refresh_from_db()
-        # Проверяем статус ответа
+        # Проверяем что рейтинг книги расчитался правильно
         self.assertEqual(str(self.book.rating), response.data['rating'])
